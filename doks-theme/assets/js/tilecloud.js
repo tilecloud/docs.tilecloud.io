0677f6df19ae1565afda2f599983b1e414e1a3d1
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-04-14 09:53:54 +0900

    some fix

---
+++ doks-theme/assets/js/tilecloud.js
@@ -0,0 +1,19 @@
+( function( $ ) {
+
+  if ( $( '.tilecloud-styles' ).length ) {
+    var imgUrl = 'https://styles.tilecloud.io/:style/screenshot.png'
+    $.ajax( { url: 'https:/api.tilecloud.io/v1/styles' } )
+      .done( function( data ) {
+        for ( var key in data ) {
+          var style = $( '<div class="style"></div>' )
+          var img = $('<div><img src="' + imgUrl.replace( ':style', key ) + '" /></div>')
+          var name = $('<div>' + key + '</div>')
+          style.append( img )
+          style.append( name )
+
+          $( '.tilecloud-styles' ).append( style )
+        }
+      } )
+  }
+
+} )( jQuery );
