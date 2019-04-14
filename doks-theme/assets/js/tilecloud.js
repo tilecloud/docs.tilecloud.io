790458001c98a56f02a6e128f1e8de0ec9040ac4
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-04-14 21:10:32 +0900

    bug fix for js

--- doks-theme/assets/js/tilecloud.js
+++ doks-theme/assets/js/tilecloud.js
@@ -2,7 +2,7 @@
 
   if ( $( '.tilecloud-styles' ).length ) {
     var imgUrl = 'https://styles.tilecloud.io/:style/screenshot.png'
-    $.ajax( { url: 'https:/api.tilecloud.io/v1/styles' } )
+    $.ajax( { url: 'https://api.tilecloud.io/v1/styles' } )
       .done( function( data ) {
         for ( var key in data ) {
           var style = $( '<div class="style"></div>' )
